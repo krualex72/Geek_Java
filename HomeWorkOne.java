a8c99395af9fe53468c076cfe698887f8e8a7d63
krualex72 <97018474+krualex72@users.noreply.github.com>
2022-01-14 11:48:47 +0300

    Update HomeWork One

--- HomeWorkOne.java
+++ HomeWorkOne.java
@@ -1,51 +1,47 @@
-public class HomeWorkOne {
+/**
+ * Java 1. HomeWork 1
+ * 
+ * @author Alexey Krutikov
+ * @version 14.01.2022
+*/
+class HomeWorkOne {
     public static void main(String[] args) {
-		System.out.println("### Section 2 ###");
-		printThreeWords();
-		System.out.println("\n### Section 3 ###");
-		checkSumSign();
-		System.out.println("\n### Section 4 ###");
-		printColor();
-		System.out.println("\n### Section 5 ###");
-		compareNumbers(); 
-	}
+        System.out.println("### Section 2 ###");
+        printThreeWords();
+        System.out.println("\n### Section 3 ###");
+        checkSumSign();
+        System.out.println("\n### Section 4 ###");
+        printColor();
+        System.out.println("\n### Section 5 ###");
+        compareNumbers(); 
+    }
 
-	public static void printThreeWords() {
-		System.out.println("Orange");
-		System.out.println("Banana");
-		System.out.println("Apple");
-	}
+    static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
 
-	public static void checkSumSign() {
-		int a = 5;
-		int b = -12;
-		if ((a+b) >= 0) {
-			System.out.println("The Summ is Positive");
-			} else {
-				System.out.println("The Summ is Negative");
-			}
-	}
+    static void checkSumSign() {
+        int a = 5;
+        int b = -12;
+        System.out.println("The Summ is " + ((a+b) >= 0 ? "Positive" : "Negative"));
+    }
 
-	public static void printColor() {
-		int value = 700;
-		if (value <= 0) {
-			System.out.println("Red");
-		} else {
-			if (value > 0 && value <= 100 ) {
-				System.out.println("Yellow");
-				} else {
-					System.out.println("Green");
-				}
-		}
-	}
+    static void printColor() {
+        int value = 700;
+        if (value <= 0) {
+            System.out.println("Red");
+        } else if (value > 0 && value <= 100 ) {
+            System.out.println("Yellow");
+        } else {
+            System.out.println("Green");
+            }
+        }
 
-	public static void compareNumbers() {
-		int a = -15;
-		int b = -5;
-		if (a >= b) {
-			System.out.println("a >= b");
-			} else {
-				System.out.println("a < b");
-				}
-	}
+    static void compareNumbers() {
+        int a = -15;
+        int b = -5;
+        System.out.println(a >= b ? "a >= b" : "a < b");
+    }
 }
