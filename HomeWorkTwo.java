d339582b6bdedf7aa6a907582c7e90d97bea8329
krualex72 <97018474+krualex72@users.noreply.github.com>
2022-01-17 19:43:21 +0300

    HomeWork 2 - corrected code

--- HomeWorkTwo.java
+++ HomeWorkTwo.java
@@ -2,16 +2,16 @@
  * Java 1. HomeWork 2
  * 
  * @author Alexey Krutikov
- * @version 14.01.2022
+ * @version 14.01.2022 - 17.01.2022 (last correction)
 */
 class HomeWorkTwo {
     public static void main(String[] args) {
         System.out.println("--- Section 1 ---");
-        System.out.println(intSumm10to20(5, 15));
+        System.out.println(intSumm10to20(5, -15));
         System.out.println("\n--- Section 2 ---");
         positiveOrNegative(0);
         System.out.println("\n--- Section 3 ---");
-        System.out.println(booleanPositiveOrNegative(-1));
+        System.out.println(booleanPositiveOrNegative(-10));
         System.out.println("\n--- Section 4 ---");
         printNStrings("So cool", 3);
         System.out.println("\n--- Section 5* ");
@@ -20,17 +20,17 @@ class HomeWorkTwo {
 
     static boolean intSumm10to20(int a, int b) {
         // сумма лежит в пределах от 10 до 20 (включительно)? Если да – вернуть true, в противном случае – false
-        return ((a + b) >= 10 && (a + b) <=20) ? true : false;
+        return ((a + b) > 9 && (a + b) <21);
     }
     
     static void positiveOrNegative(int c) {
         // напечатаем в консоль, положительное ли число передали или отрицательное
-        System.out.println (c >= 0 ? "Positive" : "Negative");
+        System.out.println (c > -1 ? "Positive" : "Negative");
     }
     
     static boolean booleanPositiveOrNegative(int c) {
         // вернуть true, если число отрицательное, и вернуть false если положительное
-        return c >= 0 ? false : true;
+        return c > -1 ;
     }
     
     static void printNStrings(String text, int n) {
